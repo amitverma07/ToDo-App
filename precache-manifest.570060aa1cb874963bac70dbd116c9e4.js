24f3e509579d9d1589eec1ddf689fe7beaf4c6d1
AMIT VERMA <55926153+amitverma07@users.noreply.github.com>
2020-04-04 22:31:47 -0500

    Updates

---
+++ precache-manifest.570060aa1cb874963bac70dbd116c9e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74358877e80c858bd362f1f368b27f49",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "7872aef900bafd33eb53",
+    "url": "/ToDo-App/static/css/main.d9724743.chunk.css"
+  },
+  {
+    "revision": "ef9c2c8eab5799bcf75c",
+    "url": "/ToDo-App/static/js/2.a1955ec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-App/static/js/2.a1955ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7872aef900bafd33eb53",
+    "url": "/ToDo-App/static/js/main.b0ada5d0.chunk.js"
+  },
+  {
+    "revision": "3827217b63fcf4c94f59",
+    "url": "/ToDo-App/static/js/runtime-main.75a3be0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDo-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
